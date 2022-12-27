2cc5096b6442c63e10d30a9dd3ab02ce5a230b65
Thiago Felipe Peçanha <thiago@augment.co>
2022-12-27 16:58:32 -0300

    Daemon/Serverless to handle Shoe Sales stuff

---
+++ libs/clients/webservice_client.rb
@@ -0,0 +1,21 @@
+require 'faye/websocket'
+require 'eventmachine'
+require 'json'
+
+require_relative '../../app/workers/persist_shoe_sales_worker'
+
+Thread.new do
+  EM.run do
+    logger ||= Logger.new(STDOUT)
+    ws = Faye::WebSocket::Client.new("#{ENV.fetch('WEBSOCKET_URL')}:#{ENV.fetch('WEBSOCKET_PORT')}")
+
+    ws.on :message do |event|
+      if (data = JSON.parse(event.data))
+        logger.info "Received: #{data}"
+        ::PersistShoeSalesWorker.perform_later(data)
+      end
+    end
+  rescue StandardError => e
+    logger.error("WebserviceClient:: An unexpected Error occurred: #{e}")
+  end
+end
---
+++ server_control.rb
@@ -0,0 +1,3 @@
+require 'daemons'
+
+Daemons.run('libs/clients/webservice_client.rb')
